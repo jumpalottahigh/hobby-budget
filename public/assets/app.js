c9a232e6b889b875fbcd4a4a6c8c50b16802e3d1
Georgi Yanev <georgiyanev.gy@gmail.com>
2016-05-21 12:24:33 +0300

    refactor login for firebase 3.0; add grand total sumns

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,52 +1,65 @@
-//DEV
-var firebaseDB = new Firebase("https://boiling-heat-4669.firebaseio.com/hobby-budget/");
-//PRODUCTION
-// var firebaseDB = new Firebase("https://hobby-budget.firebaseio.com");
+// Initialize Firebase
+//Production
+// var config = {
+//   apiKey: "AIzaSyDCMODJqnwMKRwS2xI1OVugW-G5PzzNraA",
+//   authDomain: "hobby-budget.firebaseapp.com",
+//   databaseURL: "https://hobby-budget.firebaseio.com",
+//   storageBucket: "hobby-budget.appspot.com",
+// };
 
-var user = firebaseDB.getAuth();
+//Development
+var config = {
+  apiKey: "AIzaSyDJJAuimDWqUJJCrSAUexG95PwoXSuCahw",
+  authDomain: "boiling-heat-4669.firebaseapp.com",
+  databaseURL: "https://boiling-heat-4669.firebaseio.com",
+  storageBucket: "boiling-heat-4669.appspot.com",
+};
 
-//Authenticate the user
-function login(username, pass) {
-  firebaseDB.authWithPassword({
-    email: username,
-    password: pass
-  }, function(error, authData) {
-    if (error) {
-      console.log("Login Failed! " + error);
+//Create app, DB ref and auth
+var app = firebase.initializeApp(config);
+var firebaseDB = app.database().ref();
+var auth = app.auth();
+
+//Check if the user is logged in
+function loggedIn() {
+  auth.onAuthStateChanged(function(user) {
+    if (user) {
+      //singed in
+      $('#loginStatus').removeClass("alert-danger").addClass("alert-success").html("You are logged in as: " + JSON.stringify(user.email));
+      $('#btnModalLogin').hide();
     } else {
-      console.log("Authenticated successfully!");
-
-      user = firebaseDB.getAuth();
-      if (user === null) {
-        //user not logged in
-        $('#loginStatus').removeClass("alert-success").addClass("alert-danger").html("Not logged in!");
-      } else {
-        //user logged in
-        $('#loginStatus').removeClass("alert-danger").addClass("alert-success").html("You are logged in as: " + JSON.stringify(user.password.email));
-      }
-
-      location.reload();
+      //signed out
+      $('#loginStatus').removeClass("alert-success").addClass("alert-danger").html("Not logged in!");
+      $('#btnModalLogin').show();
     }
   });
 }
 
-//Check if the user is logged in
-(function loggedIn() {
-  user = firebaseDB.getAuth();
-  //Check if user is logged in
-  if (user === null) {
-    //user not logged in
-    $('#loginStatus').removeClass("alert-success").addClass("alert-danger").html("Not logged in!");
-    $('#btnModalLogin').show();
-  } else {
-    //user logged in
-    $('#loginStatus').removeClass("alert-danger").addClass("alert-success").html("You are logged in as: " + JSON.stringify(user.password.email));
-    $('#btnModalLogin').hide();
-  }
-})();
+//Check user login status at start up
+loggedIn();
+
+//Authenticate the user
+function login(username, pass) {
+  //REFACTOR LOGIN TO 3.0
+  auth.signInWithEmailAndPassword(username, pass).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode + errorMessage);
+    // ...
+    if (errorCode === 'auth/wrong-password') {
+      console.log('Wrong password.');
+    } else {
+      console.error(error);
+    }
+    console.log("SUCCESS?");
+  });
+}
+
+
 
 function validateInput(input) {
-  if(input === "") {
+  if (input === "") {
     return false;
   } else {
     return true;
@@ -54,18 +67,18 @@ function validateInput(input) {
 }
 
 //Grab initial data from DB and populate UI
-firebaseDB.on("value", function (snap) {
+firebaseDB.on("value", function(snap) {
   var currentTimestamp = Date.now();
 
   //First time start up initializers
   //If user has no balance, let's set up the app for first use
-  if(snap.val().balance === undefined) {
+  if (snap.val().balance === undefined) {
     //Create a recurring payment at first date
     var startingDate = currentTimestamp;
     var nextDate = moment(startingDate).add(30, 'days').unix() * 1000;
 
     var initBalance;
-    if($('#monthlyAllowance').val() === "") {
+    if ($('#monthlyAllowance').val() === "") {
       initBalance = 0;
     } else {
       initBalance = parseInt($('#monthlyAllowance').val());
@@ -79,7 +92,7 @@ firebaseDB.on("value", function (snap) {
   }
 
   //Add balance to the user if a monthy has passed
-  if(parseInt(currentTimestamp) > parseInt(snap.val().nextPaymentDate)){
+  if (parseInt(currentTimestamp) > parseInt(snap.val().nextPaymentDate)) {
     var updateBalance = parseInt(snap.val().balance);
     var updateNextPaymentDate = snap.val().nextPaymentDate;
     updateNextPaymentDate = moment(updateNextPaymentDate).add(30, 'days').unix() * 1000;
@@ -98,12 +111,14 @@ firebaseDB.on("value", function (snap) {
   var constructorTotals = '';
   var totalThisMonth = 0;
   var totalThisYear = 0;
+  var grandTotalMonth = 0;
+  var grandTotalYear = 0;
 
   for (var i in allPurchases) {
     constructorPurchases += "<h2 class='flex-row text-info'>" + i + "</h2><div class='flexbox-container margin-top'>";
 
     //Collect totals information
-    constructorTotals += '<div class="col-xs-4">' + i + "</div>";
+    constructorTotals += '<div class="col-xs-4">' + i + '</div>';
 
     for (var j in allPurchases[i]) {
       //Purchase history
@@ -112,11 +127,11 @@ firebaseDB.on("value", function (snap) {
       constructorPurchases += '</div>';
 
       //Check whether to add purchase to current month or current year
-      if(moment(currentTimestamp).format("MMM") == moment(allPurchases[i][j].timestamp).format("MMM")) {
+      if (moment(currentTimestamp).format("MMM") == moment(allPurchases[i][j].timestamp).format("MMM")) {
         //Purchse was done in the current month
         totalThisMonth += parseFloat(allPurchases[i][j].price);
       }
-      if(moment(currentTimestamp).format("YYYY") == moment(allPurchases[i][j].timestamp).format("YYYY")) {
+      if (moment(currentTimestamp).format("YYYY") == moment(allPurchases[i][j].timestamp).format("YYYY")) {
         //Purchase in the current year
         totalThisYear += parseFloat(allPurchases[i][j].price);
       }
@@ -126,12 +141,21 @@ firebaseDB.on("value", function (snap) {
     //Fix to 2 decimal places
     constructorTotals += '<div class="col-xs-4 text-success"><strong>' + totalThisMonth.toFixed(2) + '</strong></div><div class="col-xs-4 text-info"><strong>' + totalThisYear.toFixed(2) + '</strong></div>';
 
+    //Add the current total to the grand total
+    grandTotalMonth += totalThisMonth;
+    grandTotalYear += totalThisYear;
+
     //Reset total counters for next itiration
     totalThisMonth = 0;
     totalThisYear = 0;
   }
 
+  // console.log(grandTotalMonth);
+  // console.log(grandTotalYear);
+  constructorGrandTotal = '<div class="col-xs-4"><strong>Grand Total</strong></div><div class="col-xs-4 text-success"><strong>' + grandTotalMonth.toFixed(2) + '</strong></div><div class="col-xs-4 text-info"><strong>' + grandTotalYear.toFixed(2) + '</strong></div>';
+
   //Update the UI
+  $('#grand-total').html(constructorGrandTotal);
   $('#totals').html(constructorTotals);
   $('#purchases').html(constructorPurchases);
   $('#monthlyAllowance').val(snap.val().monthlyAllowance);
@@ -143,12 +167,12 @@ firebaseDB.on("value", function (snap) {
 //UI
 //
 //Login button
-$('#btnLogin').click(function () {
+$('#btnLogin').click(function() {
   login($('#user').val(), $('#pass').val());
 });
 
 //Save settings button
-$('#btnSaveSettings').click(function () {
+$('#btnSaveSettings').click(function() {
   var allowance = parseInt($('#monthlyAllowance').val());
   firebaseDB.update({
     monthlyAllowance: allowance
@@ -156,13 +180,13 @@ $('#btnSaveSettings').click(function () {
 });
 
 //Add expense button
-$('#btnAddExpense').click(function () {
+$('#btnAddExpense').click(function() {
   var exName = $('#expenseName').val();
   var exPrice = $('#expenseValue').val();
   var exCategory = $('#expenseCategory').val();
   var date = Date.now();
 
-  if(validateInput(exName) && validateInput(exPrice) && validateInput(exCategory)) {
+  if (validateInput(exName) && validateInput(exPrice) && validateInput(exCategory)) {
     //Save the purchase to FB
     firebaseDB.child('/purchases/' + exCategory).push({
       name: exName,
@@ -172,7 +196,7 @@ $('#btnAddExpense').click(function () {
 
     //Update the current balance
     var oldBalance = '';
-    firebaseDB.once('value', function (snap) {
+    firebaseDB.once('value', function(snap) {
       oldBalance = snap.val().balance;
     });
 
