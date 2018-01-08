1d88fa5158cf06aa1f0f2680b506879a4f85336f
Georgi Yanev <georgiyanev.gy@gmail.com>
2018-01-09 00:03:19 +0200

    Clean up

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import './Header.css'
 import logo from './logo.png'
-import github from './github.svg'
-import twitter from './twitter.svg'
 
 class Header extends Component {
   render() {
@@ -20,14 +18,6 @@ class Header extends Component {
           {/* /about (goes to old page full length) */}
           <Link to="/about">About</Link>
           <a href="/blog">Blog</a>
-        </nav>
-        <nav className="social">
-          <a href="https://github.com/jumpalottahigh">
-            <img src={github} alt="GitHub logo" />
-          </a>
-          <a href="https://twitter.com/jumpalottahigh">
-            <img src={twitter} alt="Twitter logo" />
-          </a>
         </nav>
       </header>
     )
